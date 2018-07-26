b034c6627e751fd6a95368d5991163eba13c729b
hai <461215605@qq.com>
2018-07-26 18:49:26 +0800

    1

---
+++ application/admin/controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace app\admin\controller;
+use think\Controller;
+
+class IndexController extends Controller{
+	public function index(){
+
+		return $this->fetch();
+	}
+
+	public function left(){
+
+		return $this->fetch();
+	}
+
+	public function top(){
+
+		return $this->fetch();
+	}
+
+	public function main(){
+
+		return $this->fetch();
+	}
+}
